50e98f739224ef6856e886691b5b79a6f6ff24fe
Shrey Desai <shreydesai@me.com>
2017-05-18 10:32:53 -0700

    created file for general utilities

--- ica/utils.py
+++ ica/utils.py
@@ -1,6 +1,25 @@
 import json
 
 
+def get_file_extension(filename):
+    """
+    Extracts file extension from a file name
+    Ex: png from pfpic.png
+    """
+    return filename.rsplit('.', 1)[1].lower()
+
+
+def allowed_filename(filename, whitelist):
+    """Determines if the file extension is in the whitelist""" 
+    return '.' in filename and \
+           get_file_extension(filename) in whitelist
+
+
+def generate_token():
+    """Generates unique profile picture token"""
+    return uuid.uuid4().hex
+
+
 def jsonify_response(resp):
     """
     Converts a Flask Response object to a deserialized
